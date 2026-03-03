a1dedfbd30f4858ca2e15ce462144cf7c6af7236
Eje Obed Honour <120287165+HonourObed@users.noreply.github.com>
2026-03-03 13:03:33 +0100

    Updated News section to include the IDEA Grant and MLC

--- constants.ts
+++ constants.ts
@@ -51,69 +51,81 @@ export const PROJECTS: Project[] = [
 ];
 
 export const NEWS_ITEMS: NewsItem[] = [
+ { 
+    id: '13', 
+    date: 'Feb 2026', 
+    content: 'Won the IDEA Travel Grant for fully funded participation at the IEEE International Conference on Robotics and Automation (ICRA 2026)',
+  },
+  
   { 
-    id: '1', 
+    id: '13', 
+    date: 'Feb 2026', 
+    content: 'Joined MLC as an Independent Researcher and member of the Robotics Focus Group',
+  },
+  
+  { 
+    id: '12', 
     date: 'Jan 2026', 
     content: 'Developed AgroScout, an autonomous precision agriculture robot integrating learning-based visual perception with probabilistic navigation and real-time control.',
     link: 'https://github.com/HonourObed/agriscout_robot'
   },
   { 
-    id: '2', 
+    id: '11', 
     date: 'Jan 2026', 
     content: 'Delivered a departmental lecture on research consistency and long-term academic development, discussing structured technical growth.' 
   },
   { 
-    id: '3', 
+    id: '10', 
     date: 'Aug 2025', 
     content: 'Developed a Smart Fire Detection and Response System, combining embedded sensing, rule-based decision logic, and constrained real-time execution.',
     link: 'https://github.com/HonourObed/Smart-Fire-Detection-and-Extinguishing-System'
   },
   { 
-    id: '4', 
+    id: '9', 
     date: 'June 2025', 
     content: 'Awarded the NNPC–Chevron Joint Venture Undergraduate Scholarship.' 
   },
   { 
-    id: '5', 
+    id: '8', 
     date: 'June 2025', 
     content: 'Developed MediBot, a real-time teleoperated mobile robot exploring low-latency communication and human-in-the-loop control.',
     link: 'https://github.com/HonourObed/MediBot--Web-Teleoperated-Hospital-Logistics-Robot'
   },
   { 
-    id: '6', 
+    id: '7', 
     date: 'June 2025', 
     content: 'Contributed to the development of a machine-learning-based Anxiety Detection System under the Green Wireless Networking Group (GWIN), focusing on physiological signal acquisition and uncertainty in real-time inference.' 
   },
   { 
-    id: '7', 
+    id: '6', 
     date: 'Jan 2025', 
     content: 'Awarded the Petroleum Trust Fund (PTDF) In-Country Scholarship (ISS).' 
   },
   { 
-    id: '8', 
+    id: '5', 
     date: 'Jan 2025', 
     content: 'Secured 5th Place at PAADC 8.0 as Technical Lead of Team Enactus FUTMinna, overseeing system architecture and technical implementation.',
     link: 'https://drive.google.com/file/d/1DVtbSGGjaSjrCkbZ0eWDeIc8wl-d1x5h/view?usp=sharing'
   },
   { 
-    id: '9', 
+    id: '4', 
     date: 'Dec 2024', 
     content: 'Appointed Technical Lead of the MATLAB Student Community, FUTMinna, organizing structured workshops in modeling and computational systems.',
     link: 'https://www.linkedin.com/company/futminna-matlab-space/?lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3B0hzBv4YlRsmkiGOs2SBU5w%3D%3D'
   },
   { 
-    id: '10', 
+    id: '3', 
     date: 'Nov 2024', 
     content: 'Received an Honourable Mention for “Tidiest Interface” at the National MATLAB Competition.' 
   },
   { 
-    id: '11', 
+    id: '2', 
     date: 'April 2024', 
     content: 'Joined the Green Wireless Networking Group under Professor Elizabeth Onwuka, contributing to research in AI-enabled physiological monitoring systems.',
     link: 'https://www.researchgate.net/scientific-contributions/Elizabeth-N-Onwuka-2120992756'
   },
   { 
-    id: '12', 
+    id: '1', 
     date: 'Dec 2023', 
     content: 'Joined the Advanced Engineering Innovation Research Group (AEIRG), initiating formal research engagement in intelligent systems.',
     link: 'https://aeirg.com.ng/'
